961f5cbedc80af698eb30c9422b5fd3e0e0cafab
William Villeneuve <ciiqr@users.noreply.github.com>
2022-11-02 23:45:40 -0400

    added release script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+declare commit
+commit="$(git rev-parse --short HEAD)"
+
+gh release create \
+    --title "$commit" \
+    --notes '' \
+    "v-${commit}" \
+    'jq'
